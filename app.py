55553c9f40171ec87a17f2cc6e3d9c582746b029
Mariana Ahmar <128077807+marahmarx@users.noreply.github.com>
2025-06-17 22:25:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -384,7 +384,8 @@ if "score_esg" in st.session_state and "score_fin" in st.session_state:
                     y=valores_ebitda,
                     mode='lines+markers+text',
                     name=f'EBITDA - {nome}',
-                    text=[f"R$ {v:.2f} Bi" if i == 5 and i == 1 else "" for i,  v in enumerate(valores_ebitda)],
+                    text=[f"R$ {v:.2f} Bi" if i == 5 else "" for i,  v in enumerate(valores_ebitda)],
+                    text=[f"R$ {v:.2f} Bi" if i == 1 else "" for i,  v in enumerate(valores_ebitda)],
                     textposition="top center"
                 ))
             
@@ -393,7 +394,8 @@ if "score_esg" in st.session_state and "score_fin" in st.session_state:
                     y=valores_lucro,
                     mode='lines+markers+text',
                     name=f'Lucro Líquido - {nome}',
-                    text=[f"R$ {v:.2f} Bi" if i == 5 and i == 1 else "" for i, v in enumerate(valores_lucro)],
+                    text=[f"R$ {v:.2f} Bi" if i == 5  else "" for i, v in enumerate(valores_lucro)],
+                    text=[f"R$ {v:.2f} Bi" if i == 1  else "" for i, v in enumerate(valores_lucro)],
                     textposition="bottom center"
                 ))
 
